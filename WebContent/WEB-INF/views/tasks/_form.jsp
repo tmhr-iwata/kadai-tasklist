8ddcc3609960b1b82a77bcf760b08112b37f7bcb
tmhr.iwata <0041.gfcaus@gmail.com>
2019-11-21 13:27:01 +0900

    Modify Index/NewServlet

---
+++ WebContent/WEB-INF/views/tasks/_form.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<label for="title">タスク名</label><br />
+<input type="text" name="title" value="${task.title}" />
+<br /><br />
+
+<label for="content">内容</label><br />
+<input type="text" name="content" value="${task.content}" />
+<br /><br />
+
+<label for="limitday">期日</label><br>
+<input type="date" name="limitday" value="${task.limitday}" />
+<br><br>
+
+<input type="hidden" name="_token" value="${_token}" />
+<input type="hidden" name="completed" value="${task.completed}" />
+<button type="submit">登録</button>
--- src/models/Task.java
+++ src/models/Task.java
@@ -37,7 +37,7 @@ public class Task {
     private Timestamp limitday; // タスクの締め切り日
 
     @Column(name = "completed", nullable = false)
-    private Integer completed; // 0で未了タスク、1で完了タスク
+    private Boolean completed = false; // 0で未了タスク、1で完了タスク
 
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
@@ -77,11 +77,11 @@ public class Task {
         this.limitday = limitday;
     }
 
-    public Integer getCompleted() {
+    public Boolean getCompleted() {
         return completed;
     }
 
-    public void setCompleted(Integer completed) {
+    public void setCompleted(Boolean completed) {
         this.completed = completed;
     }
 
