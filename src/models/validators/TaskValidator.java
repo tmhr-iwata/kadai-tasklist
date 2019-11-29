6c9036c65a567c17c1c94d640ce53c09152d3cd9
tmhr.iwata <0041.gfcaus@gmail.com>
2019-11-29 10:11:44 +0900

    Modify validation

--- src/models/validators/TaskValidator.java
+++ src/models/validators/TaskValidator.java
@@ -1,6 +1,5 @@
 package models.validators;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +18,7 @@ public class TaskValidator {
         if (!content_error.equals("")){
             errors.add(content_error);
         }
-
+/*
         SimpleDateFormat sdfDay = new SimpleDateFormat("yyyy-MM-dd");
         SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm:ss");
         String fmtedDay = null;
@@ -28,16 +27,12 @@ public class TaskValidator {
             fmtedDay = sdfDay.format(t.getLimitday());
             fmtedTime = sdfTime.format(t.getLimitday());
         }
-        String fLimitday = null;
-        if (fmtedDay == null || fmtedTime == null){
-            fLimitday = "1970/01/01 00:00:00";
-        }
-        fLimitday = fmtedDay + fmtedTime + ":00";
+        String fLimitday = fmtedDay + fmtedTime + ":00";
         String limitday_error = _validateLimitday(fLimitday);
         if (!limitday_error.equals("")){
             errors.add(limitday_error);
         }
-
+*/
         return errors;
     }
 
@@ -57,6 +52,9 @@ public class TaskValidator {
         return "";
     }
 
+/*  期日もバリデーション実装を試みたが、<input type="date">が値無しの場合の実装方法がわからなく、
+ *  html側で入力必須にするrequiredで実装する形にしました。
+ *  非対応ブラウザは今日日そんなにないとは思いますが。。。
     private static String _validateLimitday(String limitday) {
         if(limitday == null || limitday.equals("")) {
             return "タスクの期日を入力してください。";
@@ -64,4 +62,5 @@ public class TaskValidator {
 
         return "";
     }
+*/
 }
